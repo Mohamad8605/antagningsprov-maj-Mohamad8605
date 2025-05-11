98103ce83c08860b5766d373adb8d7695fb6f584
Mohamad Alheresh <samirmohamad201428@gmail.com>
2025-05-11 14:20:28 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -2,11 +2,15 @@
 function uppg4() {
   
     // deklarera ett objekt med namnet person
+    const person = {};
+  
     // objektet ska ha två properties, name och age
+    person.name = "Mohamad Alheresh";
+    person.age = 25;
+  
     // tilldela age 25
     // skriv ut med dotnotation åldern i konsolen
-
-
+  console.log(person.age);
 }
 
 module.exports = { uppg4 };

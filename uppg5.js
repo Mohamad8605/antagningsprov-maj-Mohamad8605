6445df2bbe6af683ef9f99bd92597c8917d83853
Mohamad Alheresh <samirmohamad201428@gmail.com>
2025-05-11 14:24:05 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,16 @@
 
 function uppg5() {
     // skriv en if-sats som jämför två tal
+    const tal1 = 15;
+    const tal2 = 10;
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
+    if(tal1>tal2){
+        console.log("Det första talet är större än det andra");
+    }
     // annars skriv ut "Det andra talet är större än det första"
-
-
-
+    else {
+        console.log("Det andra talet är större än det första");
+    }
 }
 
 module.exports = { uppg5 };

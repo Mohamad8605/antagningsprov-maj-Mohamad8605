71aaa4ff4ebc1690992986c6c8c6e1a28ca616f2
Mohamad Alheresh <samirmohamad201428@gmail.com>
2025-05-11 15:05:08 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -5,10 +5,24 @@ function uppg9 (){
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
     // i if-sattsen skriv ut i konsolen resultatet
     // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
-
+    
     // anropa funktionen och skicka med en array som argument
 
-  
+ function sort(numberArray) {
+     
+        for (const number of numberArray) {
+           
+            if (number % 2 === 0) {
+                console.log(`${number} jämt`);
+            } else {
+                console.log(`${number} udda`);
+            }
+        }
+    }
+    
+    // Anropar funktionen med ett exempel på array
+    sort([3, 8, 12, 5, 6]);
 }
+
 
 module.exports = { uppg9 };

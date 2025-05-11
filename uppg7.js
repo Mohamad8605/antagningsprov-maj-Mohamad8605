d76df40a92c82f1e301f8c3145939028a1ec5cb6
Mohamad Alheresh <samirmohamad201428@gmail.com>
2025-05-11 14:34:10 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -3,10 +3,12 @@
 function uppg7() {
    
     // skapa en funktion som tar in två tal som argument
+   function addera (tal1, tal2) {
     // funktionen ska returnera summan av talen
+      return tal1 + tal2;
+   }
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-
- 
+   return addera(5, 10);
 }
 
 module.exports = { uppg7 };

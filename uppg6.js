90f012c34806c906556e607dfdd86661238d2c7e
Mohamad Alheresh <samirmohamad201428@gmail.com>
2025-05-11 14:28:50 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -3,9 +3,11 @@
 function uppg6(){
 
     // Skapa en array med namn som du döper till names
+    const names = ["Robert", "Mikael", "Mohamad", "Anna", "Eva"];
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 }
 
 module.exports = { uppg6 };

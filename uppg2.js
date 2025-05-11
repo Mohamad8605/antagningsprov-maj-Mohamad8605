054ef678f65b5b3abfadb9e8492e0a5aad41eb47
Mohamad Alheresh <samirmohamad201428@gmail.com>
2025-05-11 15:11:33 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -1,7 +1,7 @@
 function uppg2(){
   // Skriv din kod här
   // Deklarera en variabel med ditt namn och skriv ut det i konsolen
-  const mittNamn = "Ditt Namn Här"; // Ersätt "Ditt Namn Här" med ditt faktiska namn
+  const mittNamn = "Mohamad Alheresh"; 
   console.log(mittNamn);
 }
 
